48607c13f38a6a7aaf24e278a5de60bd90e0fa58
Coar <coar@Mac.local>
2025-03-22 16:24:39 +0100

    Changes to serve static page to GitHub Pages

--- src/main.py
+++ src/main.py
@@ -1,17 +1,23 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
 
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
 
 def main():
+    if sys.argv[0]:
+        basepath = sys.argv[0]
+    else:
+        basepath = "/"
+
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -20,7 +26,7 @@ def main():
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
 main()
